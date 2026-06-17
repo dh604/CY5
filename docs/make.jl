1f58d1c9ecbfa642c89045e7756b69acc06e5a8b
dh604 <daniel.holmes@gmx.at>
2026-06-17 11:35:43 +0200

    Split docs into GW + PT part

--- docs/make.jl
+++ docs/make.jl
@@ -16,21 +16,27 @@ makedocs(
     format   = Documenter.HTML(mathengine = MathJax3()),
     pages    = [
         "Home"       => "index.md",
-        "Pipeline"   => "pipeline.md",
-        "Spaces" => "spaces.md",
-        "Example"   => "example.md",
-        "Conjectures" => [
-            "Overview of Conjectures" => "conj/overview.md",
-            "Conjecture 4.2" => "conj/Conj_4_2.md",
-            "Section 4.3" => "conj/Conj_4_3.md",
-            "Conjecture 5.1" => "conj/Conj_5_1.md",
-            "Conjecture 6.1" => "conj/Conj_6_1.md",
-            "Conjecture 7.1" => "conj/Conj_7_1.md",
-            "Section 7.2" => "conj/Conj_7_2.md",
-            "Section 7.3" => "conj/Conj_7_3.md",
-            "Section 7.4" => "conj/Conj_7_4.md",
-            "Section 7.5" => "conj/Conj_7_5.md",
-            "Conjecture 8.1" => "conj/Conj_8_1.md"
+        "Gromov-Witten computations" => [
+            "Pipeline"   => "pipeline.md",
+            "Spaces" => "spaces.md",
+            "Example"   => "example.md",
+            "Conjectures" => [
+                "Overview of Conjectures" => "conj/overview.md",
+                "Conjecture 4.2" => "conj/Conj_4_2.md",
+                "Section 4.3" => "conj/Conj_4_3.md",
+                "Conjecture 5.1" => "conj/Conj_5_1.md",
+                "Conjecture 6.1" => "conj/Conj_6_1.md",
+                "Conjecture 7.1" => "conj/Conj_7_1.md",
+                "Section 7.2" => "conj/Conj_7_2.md",
+                "Section 7.3" => "conj/Conj_7_3.md",
+                "Section 7.4" => "conj/Conj_7_4.md",
+                "Section 7.5" => "conj/Conj_7_5.md",
+                "Conjecture 8.1" => "conj/Conj_8_1.md"
+            ]
+        ],
+        "Pandharipande-Thomas computations" => [
+            "Overview" => "pt/overview.md"
+            "Other page" => "pt/other_page.md"
         ],
         "References" => "references.md"
     ];
---
+++ docs/src/pt/overview.md
@@ -0,0 +1,5 @@
+# Overview of PT computations
+
+_insert content here_
+
+$$a^2 + b^2$$
